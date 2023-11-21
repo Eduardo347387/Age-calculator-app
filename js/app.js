43c837883a97fb864f0c548f987b52fbcdb64e89
Eduardo347387 <143133753+Eduardo347387@users.noreply.github.com>
2023-11-21 17:27:11 -0600

    Listo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 
-
 document.addEventListener('DOMContentLoaded', function() {
     const $formulario = document.getElementById('form');
     $formulario.addEventListener('submit', obtenerDatos);
@@ -7,46 +6,107 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function obtenerDatos(e){
     e.preventDefault();
+    let numeroElement = 0
     const $day = document.getElementById('day');
     const $months = document.getElementById('months')
     const $year = document.getElementById('year')
+    
+    const fechaValida = validarForm([$day,$months,$year])
 
-   validarForm([$day,$months,$year])
+    numeroElement = Object.values(fechaValida).length
+    
+    calcular(numeroElement, fechaValida)
 
 }
 
 function validarForm(fecha){
-
+    const elementosValidos = {};
     //Evaluar campo por campo
-     fecha.forEach(e=>{
+    fecha.forEach(e=>{
         if(e.value.trim() === ''){
             alertaEstadoInput(e,'#e54a4a')
             alertaText(e.parentElement,'this field is required')
+            return
         }
-        // if(fecha[0].value.trim() !== '' && validarDia(fecha[0].value)){
-        //     console.log(`${e.id}`)
-        // }
-        else{
-            alertaEstadoInput(e,' #22b335')
-            limpiarAlerta(e.parentElement,'.msError')
+        else if(e.id === 'year' && validarAnio(e.value)){
+            alertaEstadoInput(e,'#e54a4a')
+            alertaText(e.parentElement,`must be a valid ${e.id}`)
+            return
         }
+       
+        else if(e.id === 'months' && !validarMes(e.value)){
+            alertaEstadoInput(e,'#e54a4a')
+            alertaText(e.parentElement,`must be a valid ${e.id}`)
+            return
+        }
+        else if(e.id === 'day' && !esFechaValida(fecha[2],fecha[1],fecha[0])){
+            alertaEstadoInput(e,'#e54a4a')
+            alertaText(e.parentElement,`must be a valid ${e.id}`)
+            return
+        }
+        
+        elementosValidos[e.id] = e.value;
+        alertaEstadoInput(e,' #22b335')
+        limpiarAlerta(e.parentElement,'.msError')
+        
     })
-    
+    return elementosValidos;
 }
 
-function validarDia(day) {
-    const regex = /^(0?[1-9]|[1-2][0-9]|3[0-1])$/
-    return regex.test(day)
+function calcular(numElement,fecha){
+    
+    if(numElement === 3){
+        const $day = document.getElementById('result-day')
+        const $months = document.getElementById('result-months')
+        const $year = document.getElementById('result-year')
+
+        const {day,months,year} = fecha;
+        
+        const fechaNac = new Date(year,months-1,day);
+        
+        const fechaActual = new Date();
+
+        const diferenciaMilisegundos = fechaActual - fechaNac;
+
+        const edad = new Date(diferenciaMilisegundos);
+    
+        const anios = edad.getUTCFullYear() - 1970;
+        const meses = edad.getUTCMonth();
+        const dias = edad.getUTCDate() - 1;
+
+        $day.textContent = dias;
+        $months.textContent = meses;
+        $year.textContent = anios;
+    } 
 }
+
+function esFechaValida(anio, mes, dia) {
+    anio = parseInt(anio.value)
+    mes = parseInt(mes.value)
+    dia = parseInt(dia.value)
+    const fecha = new Date(anio, mes - 1, dia);
+    return (
+        fecha.getFullYear() === anio && fecha.getMonth() === mes - 1 && fecha.getDate() === dia
+    );
+}
+
 
 function validarMes(months) {
-    // Lógica de validación para el mes
+    const regex = /^(0?[1-9]|1[0-2])$/;
+    return regex.test(months)
 }
 
-function validarAnio(year) {
-    // Lógica de validación para el año
-}
+function validarAnio(year){
+    const fechaActual = new Date()
+    const regex = /^-?0*[1-9]\d{3,}\.?\d*$/;
 
+    if(regex.test(year)){
+        return parseInt(year) > fechaActual.getFullYear()
+    }else{
+        return true
+    }
+    
+}
 
 function alertaEstadoInput(referencia,color){
     let $title, $input;
